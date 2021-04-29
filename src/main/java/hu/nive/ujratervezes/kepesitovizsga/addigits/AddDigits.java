724f0c08728ccd5974012085cf18114b0ef16567
IvanyiA <trachemys13@gmail.com>
2021-04-29 10:54:19 +0200

    Add digits

--- src/main/java/hu/nive/ujratervezes/kepesitovizsga/addigits/AddDigits.java
+++ src/main/java/hu/nive/ujratervezes/kepesitovizsga/addigits/AddDigits.java
@@ -2,7 +2,19 @@ package hu.nive.ujratervezes.kepesitovizsga.addigits;
 
 public class AddDigits {
 
-//    public int addDigits(Object o) {
-//    }
-//
+    public int addDigits(String input) {
+        if (input == null || input.isEmpty()) {
+            return -1;
+        }
+        char[] c = input.toCharArray();
+        int sumOfDigits = 0;
+        for (char c1 : c) {
+
+            if (Character.isDigit(c1)) {
+                int number = Character.getNumericValue(c1);
+                sumOfDigits += number;
+            }
+        }
+        return sumOfDigits;
+    }
 }
